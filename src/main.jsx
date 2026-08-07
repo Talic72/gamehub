d2103a90d1c9bfd5db24f1a6e51ce33fd89663df
Talic72 <EMORRISWORK72@GMAIL.COM>
2026-08-07 16:21:15 -0400

    Updated main.jsx to troubleshoot deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { PlayerProvider } from "./context/PlayerContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <PlayerProvider>
         <App />
       </PlayerProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
